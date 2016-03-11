56dcd4320b2e498e25fd92118f8036151643b533
Pavlova Olga <olga.pavlova102@gmail.com>
2016-03-11 17:07:02 +0400

    AccountBillingInfo, Invoice, Account

--- ConsoleApplication1/Integrator.cs
+++ ConsoleApplication1/Integrator.cs
@@ -71,7 +71,7 @@ namespace IntegrationService
             double res = (double)_type1C.InvokeMember(@"НайтиПоНомеру", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { number, date, "1" });
             if (res > 0)
             {
-                var current = _type1C.InvokeMember("ТекущийЭлемент", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { });
+                var current = _type1C.InvokeMember("ВыбратьСтроки", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { });
                 return current;
             }
             return null;
@@ -80,13 +80,30 @@ namespace IntegrationService
         {
             _type1C.InvokeMember("УстановитьАтрибут", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { attributeName, value });
         }
-
+        private void SetDate(object obj) 
+        {
+            _type1C.InvokeMember("ИспользоватьДату", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { DateTime.Now.Date });
+        }
         private void OpenTable(object obj)
         {
             _type1C.InvokeMember("НоваяСтрока", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { });
         }
-        private void SetCode(object obj) {
+        private void SetCode(object obj) 
+        {
             _type1C.InvokeMember(@"УстановитьНовыйКод", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { "1" });
+        }
+        private void DeleteTable(object obj)
+        {
+            _type1C.InvokeMember(@"УдалитьСтроки", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { });
+        }
+        private object GetTable(object obj)
+        {
+            double res = (double)_type1C.InvokeMember(@"ПолучитьСтроку", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { });
+            if (res > 0)
+            {
+                var current = _type1C.InvokeMember("ПолучитьСтроку", BindingFlags.Public | BindingFlags.InvokeMethod, null, obj, new object[] { });
+                return current;
+            } return null;
         }
         /// <summary>
         /// Product
@@ -96,37 +113,56 @@ namespace IntegrationService
         /// <param name="code1c"></param>
         private void UpdateProduct(object product1C, Product p, string code1c) 
         {
+            SetDate(product1C);
             Set(product1C, "Артикул", code1c);
             Set(product1C, "Наименование", p.Name);
             Set(product1C, "ПолнНаименование", p.FullName);
-            Set(product1C, "ТипНоменклатуры", p.OfferingType.Code1C);
-            var offering = FindByCode(product1C, p.ActivityType.Code1C);
+            object offering1C=Open("Справочник.ВидыНоменклатуры");
+            var offering = FindByCode(offering1C, p.ActivityType.Code1C);
             if (offering != null)
             {
                 Set(product1C, "ВидНоменклатуры", offering);
             }
-            var unit = FindByProperty(product1C, "ПолнНаименование", p.Unit.Name);
+            Set(product1C, "ТипНоменклатуры", p.OfferingType.Code1C);
+            object unit1C = Open("Справочник.ЕдиницыИзмерений");
+            var unit = FindByCode(unit1C, "796");
             if (unit != null)
             {
                 Set(product1C, "ЕдиницаИзмерения", unit);
             }
+            object tax1C = Open("Справочник.СтавкиНДС");
+            if (p.Tax!=null)
+            {
+                var tax = FindByProperty(tax1C, "Ставка", p.Tax.Percent.ToString());
+                if (tax != null)
+                {
+                    Set(product1C, "СтавкаНДС", tax);
+                }
+            }
+            //Set(product1C,"Цена",price);
+            object currency1C = Open("Справочник.Валюты");
+           /* var currency = FindByProperty(currency1C, "ПолнНаименование", "Рубли");
+            if (currency != null)
+            {
+                Set(product1C, "Валюта", currency);
+            }*/
             Save(product1C);
         }
-        private void SyncProduct(Invoice i) 
+        private void SyncProduct(Invoice i, int countproduct) 
         {
-            string code1c=i.Products[0].Product.Code1C;
+            string code1c = i.Products[countproduct].Product.Code1C;
             object product=Open("Справочник.Номенклатура");
             var curproduct = FindByProperty(product, "Артикул", code1c);
             if (curproduct != null)
             {
-                UpdateProduct(product, i.Products[0].Product, code1c);
+                UpdateProduct(product, i.Products[countproduct].Product, code1c);
 
             }
             else
             {
                 Create(product);
                 SetCode(product);
-                UpdateProduct(product, i.Products[0].Product, code1c);
+                UpdateProduct(product, i.Products[countproduct].Product, code1c);
                 
             }
         
@@ -146,7 +182,8 @@ namespace IntegrationService
             Set(account1C, "ИНН", a.BillingInfo.INN + '/' + a.BillingInfo.KPP);
             Set(account1C, "ЮридическийАдрес", a.JurAddress.FullAddress);
             Set(account1C, "ПочтовыйАдрес", a.PostAddress.FullAddress);
-            Set(account1C, "ВидКонтрагента", a.Type);
+            object en = Open("Перечисление.ВидыКонтрагентов.Организация");
+            Set(account1C, "ВидКонтрагента", en);
 
             Save(account1C);
         
@@ -205,13 +242,16 @@ namespace IntegrationService
         {
             object accbill = Open("Справочник.яКонтрагенты");
 
-            var current = _type1C.InvokeMember("ИспользоватьВладельца", BindingFlags.Public | BindingFlags.InvokeMethod, null, accbill, new object[] { curaccount });
-            if (current != null)
+           // var current = _type1C.InvokeMember("ИспользоватьВладельца", BindingFlags.Public | BindingFlags.InvokeMethod, null, accbill, new object[] { curaccount });
+            var currentabi = FindByCode(accbill, abi.Code1C);
+            if (currentabi != null)
             {
-                Create(accbill);
                 UpdateAccountBilling(accbill, abi, curaccount);
             }
-            else { UpdateAccountBilling(accbill, abi, curaccount); }
+            else {
+                Create(accbill);
+                UpdateAccountBilling(accbill, abi, curaccount); 
+            }
         
         }
         private void UpdateCalculateInvoice(object acbil1C, AccountBillingInfo abi, object curaccount) 
@@ -219,19 +259,58 @@ namespace IntegrationService
             Set(acbil1C, "Номер", abi.RS);
             Set(acbil1C, "Владелец", curaccount);
             Set(acbil1C, "Наименование", "Основной");
-            Set(acbil1C, "БанкОрганизации", abi.Bank);
+            SyncBanks(abi);
+            object bank = Open("Справочник.Банки");
+            var curbank = FindByName(bank, abi.Bank);
+            if (curbank != null)
+            {
+                Set(acbil1C, "БанкОрганизации", curbank);
+            }
             SetCode(acbil1C);
+            Save(acbil1C);
+        }
+        private void SyncBanks(AccountBillingInfo abi) 
+        {
+            object bank = Open("Справочник.Банки");
+            var curbank = FindByName(bank, abi.Bank);
+            if (curbank != null)
+            {
+                UpdateBank(bank, abi);
+            }
+            else {
+                Create(bank);
+                UpdateBank(bank, abi);
+            }
+        
+        }
+        private void UpdateBank(object bank,AccountBillingInfo abi) 
+        {
+            Set(bank,"Наименование",abi.Bank);
+            Set(bank, "КоррСчет", abi.CorrInvoice);
+            Set(bank,"Код",abi.BIK);
+            Set(bank, "Местонахождение", ParseBankCity(abi.Bank));
+
+            Save(bank);
+        }
+        private string ParseBankCity(string bank) 
+        {
+            int start = bank.IndexOf('.');
+            string city = bank.Substring(start - 1);
+            return city;
         }
         private void SyncCalculateInvoice(AccountBillingInfo abi, object curaccount, Account a) 
         {
             object accbill = Open("Справочник.РасчетныеСчета");
-            var current = _type1C.InvokeMember("ИспользоватьВладельца", BindingFlags.Public | BindingFlags.InvokeMethod, null, accbill, new object[] { curaccount });
-            if (current != null)
+           //var current = _type1C.InvokeMember("ИспользоватьВладельца", BindingFlags.Public | BindingFlags.InvokeMethod, null, accbill, new object[] { curaccount });
+            var currentabi = FindByProperty(accbill,"Номер", abi.RS);
+            if (currentabi != null)
             {
-                Create(accbill);
                 UpdateCalculateInvoice(accbill, abi, curaccount);
             }
-            else { UpdateCalculateInvoice(accbill, abi, curaccount); }
+            else {
+                Create(accbill); 
+                UpdateCalculateInvoice(accbill, abi, curaccount);
+            }
         }
 
 
@@ -240,10 +319,37 @@ namespace IntegrationService
         /// </summary>
         /// <param name="invoice1C"></param>
         /// <param name="i"></param>
+        /// 
+        private void CreateTableInvoice(object invoice1C, Invoice i, int countproduct)
+        {
+            OpenTable(invoice1C);
+            Set(invoice1C, "Количество", i.Products[countproduct].Quantity);
+            Set(invoice1C, "Цена", i.Products[countproduct].Price);
+            Set(invoice1C, "Всего", i.Products[countproduct].TotalAmount);
+            var prodcode1c = i.Products[countproduct].Product.Code1C;
+            object product = Open("Справочник.Номенклатура");
+            var curproduct = FindByProperty(product, "Артикул", prodcode1c);
+            if (curproduct != null)
+            {
+                Set(invoice1C, "Товар", curproduct);
+            }
+            object tax1C = Open("Справочник.СтавкиНДС");
+            if (i.Products[countproduct].Product.Tax != null)
+            {
+                Set(invoice1C, "НДС", i.Products[countproduct].Product.Tax.Percent);
+                /*
+                var tax = FindByProperty(tax1C, "Ставка", i.Products[countproduct].Product.Tax.Percent.ToString());
+                if (tax != null)
+                {
+                    Set(invoice1C, "НДС", tax);
+                }*/
+            }
+        }
+        
         private void UpdateInvoice(object invoice1C, Invoice i) 
         {
             Set(invoice1C, "ДатаДок", i.StartDate);
-            Set(invoice1C, "НомерДок", i.Code1C);
+            Set(invoice1C, "НомерДок", i.Number);
             Set(invoice1C, "НомерДоговора", i.Contract.Number);
             Set(invoice1C, "ДатаДоговора", i.Contract.StartDate);
             var accountcode = i.Account.Code1C;
@@ -254,7 +360,20 @@ namespace IntegrationService
                 Set(invoice1C, "Контрагент", curaccount);
                 Set(invoice1C, "Плательщик", curaccount);
             }
-            
+            object accbill = Open("Справочник.РасчетныеСчета");
+
+            var current = FindByCode(accbill, i.Account.BillingInfo.Code1C);
+            if (current != null)
+            {
+                Set(invoice1C, "РасчетныйСчет", current);
+            }
+            int countproduct = i.Products.Count;
+            DeleteTable(invoice1C);
+            for (int k = 0; k < countproduct; k++)
+            {
+
+                CreateTableInvoice(invoice1C, i, k);
+            }
             Save(invoice1C);
         }
         private void SyncInvoice(Invoice i)
@@ -281,12 +400,14 @@ namespace IntegrationService
         public void IntegrateInvoice(Invoice i)
         {
            SyncAccount(i);
-           SyncProduct(i);
+           int countproduct = i.Products.Count;
+           for (int k=0; k < countproduct; k++) 
+           {
+               SyncProduct(i,k);
+           }
+              
            SyncInvoice(i);
-    
-            
-            
-           // Dispose();
+
         }
 
         public void Dispose()
